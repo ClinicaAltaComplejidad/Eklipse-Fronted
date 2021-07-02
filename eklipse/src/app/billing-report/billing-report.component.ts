7bb55d64453dd5ff5010e735f9e034dadc7efcf5
Edwin Molina <delgadoedwin199@gmail.com>
2021-07-02 16:56:10 -0500

    [dev] Agregando Componente BillingReport

---
+++ eklipse/src/app/billing-report/billing-report.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-billing-report',
+  templateUrl: './billing-report.component.html',
+  styleUrls: ['./billing-report.component.css']
+})
+export class BillingReportComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
