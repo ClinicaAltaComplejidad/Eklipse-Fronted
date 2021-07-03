3a04f74a06e1387d4ea7a9f27b342643a934f941
Edwin Molina <delgadoedwin199@gmail.com>
2021-07-03 11:16:03 -0500

    [dev] Agregando Modelo BillReport, Componente BillingReportComponent, servicio BillingReportService, tabla para mostrar las facturas

--- eklipse/src/app/app.module.ts
+++ eklipse/src/app/app.module.ts
@@ -6,6 +6,18 @@ import { AppComponent } from './app.component';
 import { BillingReportComponent } from './billing-report/billing-report.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+
+
+
+//componentes 
+import { MatTableModule } from "@angular/material/table";
+import { MatCheckboxModule } from "@angular/material/checkbox";
+import { MatFormFieldModule } from '@angular/material/form-field';
+
+
+
+import { HttpClientModule } from "@angular/common/http";
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -14,7 +26,11 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MatTableModule,
+    MatCheckboxModule,
+    MatFormFieldModule
   ],
   providers: [],
   bootstrap: [AppComponent]
