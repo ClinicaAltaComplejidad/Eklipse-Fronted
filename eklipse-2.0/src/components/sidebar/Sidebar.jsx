d1def399b931809dda2369b3b56d9fab47a34dc9
Rodevp <rodriguezrodrigor12@gmail.com>
2021-07-09 15:49:29 -0500

    [dev] - agregando estructura html y css del sidebar, faltan modificaciones

--- eklipse-2.0/src/components/sidebar/Sidebar.jsx
+++ eklipse-2.0/src/components/sidebar/Sidebar.jsx
@@ -1,51 +1,99 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect } from 'react'
 
-import Logo from '../../assets/img/marca_de_agua.png';
-import Arrow from '../../assets/img/right_arrow.svg';
-import Exit from '../../assets/img/exit.svg';
-
-//styles
 import './sidebar.css';
 
-function Sidebar({ children }) {
+function Sidebar() {
 
-    const [isOpen, setIsOpen] = useState(true);
+    useEffect(() => {
+        let btn = document.getElementById("btn");
+        let sidebar = document.getElementById("sidebar");
+     
+        btn.addEventListener('click', function() {
+          sidebar.classList.toggle("active");
+          if(btn.classList.contains("bx-menu")){
+            btn.classList.replace("bx-menu" , "bx-menu-alt-right");
+          }else{
+            btn.classList.replace("bx-menu-alt-right", "bx-menu");
+          }
+        });
 
-    function openSidebar() {
-        if (isOpen) {
-            setIsOpen(false);
-        } else {
-            setIsOpen(true)
-        }
-    }
+    }, []);
 
 
     return (
-        <>
-            <div className="sidebar">
-                <div className="btn_open">
-                    <input type="checkbox" id="open_btn" />
-                    <label onClick={openSidebar} htmlFor="open_btn">
-                        <img src={Arrow} alt="" width="32" loading="lazy" />
-                    </label>
+        <div className="sidebar" id="sidebar">
+            <div className="logo_content">
+                <div className="logo">
+                    <div className="logo_name">EKLIPSE 🚀</div>
                 </div>
-                <aside className={`sidebar ${isOpen ? '' : 'sidebar--close'}`} id="sidebar">
-                    <section className="sidebar_image">
-                        <img src={Logo} alt="" width="200" loading="lazy" />
-                    </section>
-                    <section className="sidebar_body">
-                        {children}
-                    </section>
-                    <footer className="sidebar_footer">
-                        <section className="footer_image">
-                            <img src={Exit} alt="" width="55" loading="lazy" />
-                        </section>
-                        <button> Salir </button>
-                    </footer>
-                </aside>
+                <i className='bx bx-menu' id="btn" ></i>
             </div>
-        </>
+            <ul className="nav_list">
+                <li>
+                    <a href="#">
+                        <i className='bx bx-grid-alt' ></i>
+                        <span className="links_name">Dashboard</span>
+                    </a>
+                    <span className="tooltip">Dashboard</span>
+                </li>
+                <li>
+                    <a href="#">
+                        <i className='bx bx-user' ></i>
+                        <span className="links_name">User</span>
+                    </a>
+                    <span className="tooltip">User</span>
+                </li>
+                <li>
+                    <a href="#">
+                        <i className='bx bx-chat' ></i>
+                        <span className="links_name">Messages</span>
+                    </a>
+                    <span className="tooltip">Messages</span>
+                </li>
+                <li>
+                    <a href="#">
+                        <i className='bx bx-pie-chart-alt-2' ></i>
+                        <span className="links_name">Analytics</span>
+                    </a>
+                    <span className="tooltip">Analytics</span>
+                </li>
+                <li>
+                    <a href="#">
+                        <i className='bx bx-folder' ></i>
+                        <span className="links_name">File Manager</span>
+                    </a>
+                    <span className="tooltip">Files</span>
+                </li>
+                <li>
+                    <a href="#">
+                        <i className='bx bx-cart-alt' ></i>
+                        <span className="links_name">Order</span>
+                    </a>
+                    <span className="tooltip">Order</span>
+                </li>
+                <li>
+                    <a href="#">
+                        <i className='bx bx-heart' ></i>
+                        <span className="links_name">Saved</span>
+                    </a>
+                    <span className="tooltip">Saved</span>
+                </li>
+                <li>
+                    <a href="#">
+                        <i className='bx bx-cog' ></i>
+                        <span className="links_name">Setting</span>
+                    </a>
+                    <span className="tooltip">Setting</span>
+                </li>
+            </ul>
+            <div className="profile_content">
+                <div className="profile">
+                    <i className='bx bx-log-out' id="log_out" ></i>
+                </div>
+            </div>
+        </div>
     )
 }
 
 export default Sidebar;
+
